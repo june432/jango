4791d1cf8020394047edb19755a73018cec79b15
lee99153926@gmail.com <lee99153926@gmail.com>
2022-07-02 12:19:57 +0900

    123

---
+++ pybo/tests.py
@@ -0,0 +1,10 @@
+from django.urls import URLPattern, path
+from . import views
+
+urlpatterns=[
+    path('',views.index),
+    path('hello/',views.index),
+    path('hello/3/',views.index),
+]
+
+# Create your tests here.
